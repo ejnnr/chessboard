516ecb5d83cc090185cd414ee08b84a53299fdfa
jupiter24 <jupiter24@users.noreply.github.com>
2014-02-22 16:52:29 +0100

    Put test area into a seperate file

--- chessboard.class.php
+++ chessboard.class.php
@@ -410,28 +410,4 @@
 		}
 		
 	}
-	
-	/* testing area */
-	
-	$board1 = new chessboard();
-	echo $board1->getHTML();
-	var_dump($board1->isValidMove("h8", "g8"));
-
 ?>
-
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-    }
-    .rank {
-        clear: both;
-    }
-	.square {
-		width: 50px;
-		height: 50px;
-		float: left;
-		font-size: 50px;
-		border: 1px solid black;
-	}
-</style>
---
+++ test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Chessboard - Test</title>
+		<meta charset="utf8">
+		<style>
+			* {
+				margin: 0;
+				padding: 0;
+			}
+			.rank {
+				clear: both;
+			}
+			.square {
+				width: 50px;
+				height: 50px;
+				float: left;
+				font-size: 50px;
+				border: 1px solid black;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+			$board1 = new chessboard("", "8/1k6/8/8/8/8/1K6/8 w - - 0 1");
+			echo $board1->getHTML();
+			var_dump($board1->isValidMove("b2", "a3"));
+		?>
+	</body>
+</html>
