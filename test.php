c5c4c0c356f13f53c696b52541ae9826b72e214b
jupiter24 <jupiter24@users.noreply.github.com>
2014-03-01 17:35:39 +0100

    test pawn instead of queen

--- test.php
+++ test.php
@@ -26,9 +26,9 @@
 	<body>
 		<?php
 			include "chessboard.class.php";
-			$board1 = new chessboard("", "8/1k6/8/8/3Qb3/8/3K4/8 w - - 0 1");
+			$board1 = new chessboard("", "8/1k6/8/2n5/3P4/8/3K4/8 w - - 0 1");
 			echo $board1->getHTML();
-			var_dump($board1->isValidMove("d4", "c2"));
+			var_dump($board1->isValidMove("d4", "c5"));
 		?>
 	</body>
 </html>
